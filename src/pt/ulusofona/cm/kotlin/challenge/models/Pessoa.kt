f27d331ab459944bf9255f5623ab4a3e867121a5
afonsocarreira22004883 <afonso1carreira@gmail.com>
2022-02-18 10:17:49 +0000

    Inicio da ficha

---
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Pessoa.kt
@@ -0,0 +1,6 @@
+package pt.ulusofona.cm.kotlin.challenge.models
+
+import java.util.*
+
+class Pessoa(var nome: String,veiculos : List<Veiculo>, dataDeNascimento : Date, carta: Carta,posicao : Posicao) {
+}

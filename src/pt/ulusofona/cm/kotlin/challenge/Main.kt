1cfa5f39d7c127bd4bb5f7f14f66313e334f0dd2
Afonso <afonso1carreira@gmail.com>
2022-02-21 18:33:52 +0000

    update

--- src/pt/ulusofona/cm/kotlin/challenge/Main.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/Main.kt
@@ -1,20 +1,9 @@
 package pt.ulusofona.cm.kotlin.challenge
 
-import pt.ulusofona.cm.kotlin.challenge.models.*
-import java.util.Date
+class Main {
 
+}
 
 fun main(){
-    val data = Date("2000/10/24")
-    val bike = Bicicleta("a")
-    val m = Motor(500,500)
-    val c = Carro("b",m)
-    val p = Pessoa("toy",data)
-    p.comprarVeiculo(c)
-    p.comprarVeiculo(bike)
-    println(p)
-    p.tirarCarta()
-    p.carta = Carta()
-    p.moverVeiculoPara("b",1,1)
 
 }
--- src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
@@ -18,7 +18,7 @@ class Posicao(var x: Int, var y: Int) {
     }
 
     override fun toString(): String {
-        return "Posicao | $x | $y"
+        return "Posicao | x:$x | y:$y"
     }
 
 
